997a318027229e41a41a50a3efa14dc1e61617d2
xshangzi <xieshangzi@gmail.com>
2025-07-06 21:21:27 +0800

    adding venv

--- webapp/install_pkg.sh
+++ webapp/install_pkg.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 project_dir=$(dirname $(dirname $(realpath $0)))
 
-# active the venv
+# Create venv if it doesn't exist
+if [ ! -d "$project_dir/.venv" ]; then
+    echo "Creating virtual environment at $project_dir/.venv..."
+    python3 -m venv $project_dir/.venv
+fi
+# activate the venv
 source $project_dir/.venv/bin/activate
 # install the requirements
 pip cache purge
